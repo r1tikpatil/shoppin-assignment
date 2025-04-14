593237daf634ec5de66b61d6fcd1cd2286d7fa3d
Ritik Patil <ritikpatil66@gmail.com>
2025-04-14 14:07:53 +0530

    fix: news api

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,6 @@
 import { apiRequest } from "./apiCall";
 
-const NEWS_URL = `https://newsapi.org/v2/everything?from=2025-03-13&q=tata&sortBy=publishedAt&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
+const NEWS_URL = `https://newsapi.org/v2/everything?from=2025-03-14&q=tata&sortBy=publishedAt&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`;
 const GOOGLE_CUSTOME_SEARCH_URL = `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_SEARCH_ENGINE_ID}&q=`;
 
 export const fetchNewsData = async () => {
